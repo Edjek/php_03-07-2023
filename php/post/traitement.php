61ff42b35783517812daeb5da060e07b2ad64739
Edjek <edjek@hotmail.fr>
2023-07-05 17:05:42 +0200

    day 3

--- php/post/traitement.php
+++ php/post/traitement.php
@@ -1,5 +1,24 @@
 <?php
 
-if(isset($_POST['name'])  && !empty($_POST['name']) && isset($_POST['pswd']) && !empty($_POST['pswd']) ){
-    
+// echo '<pre>';
+// var_dump($_POST);
+// echo '</pre>';
+
+// La faille XSS, de son nom complet Cross-Site Scripting, est une faille qui permet d'injecter du code HTML et/ou Javascript dans des variables ou bases de données mal protégées.
+
+if( isset($_POST['name']) && isset($_POST['pswd'])){
+    if( !empty($_POST['name']) || !empty($_POST['pswd'])) {
+        // La fonction strip_tags() retourne le contenu en supprimant tout élément HTML
+        $name = strip_tags($_POST['name']);
+
+        // La fonction htmlspecialchars() retourne le contenu avec les caractères spéciaux neutralisés
+        $name = htmlspecialchars($_POST['name']);
+        var_dump($name);
+    }
+    else {
+        echo 'Veuillez remplir tous les champs';
+    }
+
+} else {
+    echo 'Erreur dans le formulaire';
 }
